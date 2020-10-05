89719688a3c847787006d91d5065dc1a433e4c18
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-05 06:27:30 -0300

    Serializando Objetos java para Json

--- src/SatellitData/ClimaSatellit.java
+++ src/SatellitData/ClimaSatellit.java
@@ -1,6 +1,18 @@
 package SatellitData;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 public class ClimaSatellit {
-    
+     
+	public static Map<String, Object> jsonToMap(String str){
+		Map<String, Object> map = new Gson().fromJson(
+				str, new TypeToken<HashMap<String, Object>>() {}.getType()
+		);
+		return map;
+	}
  
 }
