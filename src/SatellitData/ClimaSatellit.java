7fb202672cf26b6c3484f64a888325ee68a474a3
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-05 06:47:01 -0300

    Adiocionando logica de acesso aos objetos e mostrando resultados do acesso

--- src/SatellitData/ClimaSatellit.java
+++ src/SatellitData/ClimaSatellit.java
@@ -1,6 +1,10 @@
 package SatellitData;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,8 +24,26 @@ public class ClimaSatellit {
 		String API_KEY = "b10a74f0720e974fc75a3e2bd810094e";
 		String LOCATION = "Raleigh,NC";
 		String urlString = "http://api.openweathermap.org/data/2.5/weather?q="+LOCATION+"&appid="+API_KEY+"&units-imperial";
-	}
 	
+	
+	 try {
+		 StringBuilder result = new StringBuilder();
+		 URL url = new URL(urlString);
+		 URLConnection conn = url.openConnection();
+		 BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		 String line;
+		 while((line = rd.readLine()) != null) {
+			 result.append(line);
+		 }
+		 
+		 rd.close();
+		 System.out.println(result);
+		 
+		 
+	 }catch (Exception e) {
+		 e.printStackTrace();
+ 	}
 	 
+	}
  
 }
